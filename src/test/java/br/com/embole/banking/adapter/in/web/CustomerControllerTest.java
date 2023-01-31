398a938fd9ee7a1459e247bc7c721446e6882599
emanuellucascomp <cardgameheritage@gmail.com>
2023-01-31 10:42:06 -0300

    adding controller tests

--- src/test/java/br/com/embole/banking/adapter/in/web/CustomerControllerTest.java
+++ src/test/java/br/com/embole/banking/adapter/in/web/CustomerControllerTest.java
@@ -1,10 +1,11 @@
 package br.com.embole.banking.adapter.in.web;
 
-import br.com.embole.banking.application.port.in.TransferUseCase;
+import br.com.embole.banking.application.port.in.CustomerUseCase;
+import br.com.embole.banking.application.port.in.request.CustomerRequest;
 import br.com.embole.banking.application.port.in.request.TransferRequest;
+import br.com.embole.banking.application.port.in.response.CustomerResponse;
 import br.com.embole.banking.application.port.in.response.TransferDetailsResponse;
 import br.com.embole.banking.application.port.in.response.TransferResponse;
-import static org.hamcrest.CoreMatchers.is;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
@@ -22,7 +23,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -32,49 +36,51 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-@WebMvcTest(value = TransferController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
-class TransferControllerTest {
+@WebMvcTest(value = CustomerController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+class CustomerControllerTest {
+
     @Autowired
     private MockMvc mvc;
     @MockBean
-    private TransferUseCase transferUseCase;
+    private CustomerUseCase customerUseCase;
 
     @Test
-    void shouldTransferMoney() throws Exception {
-        TransferResponse response = TransferResponse.builder()
-                .transferAmount(BigDecimal.valueOf(1000))
-                .sourceAccount("0000001")
-                .destinationAccount("0000002")
+    void shouldInsertCustomer() throws Exception{
+        CustomerResponse response = CustomerResponse.builder()
+                .id(UUID.randomUUID())
+                .name("Emanuel")
+                .accountNumber("0000001")
+                .balance(BigDecimal.ZERO)
                 .build();
-        TransferRequest request = TransferRequest.builder()
-                .transferAmount(BigDecimal.valueOf(1000))
-                .sourceAccount("0000001")
-                .destinationAccount("0000002")
+        CustomerRequest request = CustomerRequest.builder()
+                .name("Emanuel")
+                .accountNumber("0000001")
                 .build();
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String json = ow.writeValueAsString(request);
 
-        given(transferUseCase.transfer(Mockito.any())).willReturn(response);
+        given(customerUseCase.insertCustomer(Mockito.any())).willReturn(response);
 
-        mvc.perform(post("/api/v1/transfer").contentType(MediaType.APPLICATION_JSON)
+        mvc.perform(post("/api/v1/customer").contentType(MediaType.APPLICATION_JSON)
                         .content(json))
-                .andExpect(status().isOk());
-        verify(transferUseCase, VerificationModeFactory.times(1)).transfer(Mockito.any());
+                .andExpect(status().isCreated());
+        verify(customerUseCase, VerificationModeFactory.times(1)).insertCustomer(Mockito.any());
     }
 
     @Test
-    void shouldRetrieveTransfersFromAccount() throws Exception {
-        TransferDetailsResponse response = TransferDetailsResponse.builder()
-                .transferAmount(BigDecimal.valueOf(1000))
-                .transferDate(LocalDateTime.now())
-                .type("DEPOSIT")
+    void shouldRetrieveCustomerFromAccount() throws Exception {
+        CustomerResponse response = CustomerResponse.builder()
+                .id(UUID.randomUUID())
+                .name("Emanuel")
+                .accountNumber("0000001")
+                .balance(BigDecimal.ZERO)
                 .build();
 
-        given(transferUseCase.retrieveTransfersFromAccount(Mockito.any())).willReturn(List.of(response));
+        given(customerUseCase.getUserByAccountNumber(Mockito.any())).willReturn(Optional.of(response));
 
-        mvc.perform(get("/api/v1/transfer/accounts/0000001").contentType(MediaType.APPLICATION_JSON))
+        mvc.perform(get("/api/v1/customer/0000001").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].type",  is("DEPOSIT")));
-        verify(transferUseCase, VerificationModeFactory.times(1)).retrieveTransfersFromAccount(Mockito.any());
+                .andExpect(jsonPath("name",  is("Emanuel")));
+        verify(customerUseCase, VerificationModeFactory.times(1)).getUserByAccountNumber(Mockito.any());
     }
 }
